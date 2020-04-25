55d0a294bf65adea9fb2b4414f8539fe381c4b53
Tom Viemont <46653205+tomheartstacos@users.noreply.github.com>
2020-04-25 13:17:40 +0100

    pokemonList

---
+++ scripts.js
@@ -0,0 +1,9 @@
+var pokemonList = {
+  pokemonName, heightArray, typeArray;
+}
+//string
+var pokemonName = 'Metapod','Charmander','Squirtle';
+// array of numbers
+var heightArray = [0.7, 0.6, 0.5,];
+// array of strings
+var typeArray = ['bug', 'fire', 'water'];
