3ab11f34af3218652642d15b5b7ad3ccef441ced
Tom Viemont <46653205+tomheartstacos@users.noreply.github.com>
2020-04-27 10:16:56 +0100

    Added Conditional with threshold value

--- scripts.js
+++ scripts.js
@@ -1,18 +1,27 @@
-var pokemonList = [
-{
-name : ' Metapod ' ,
-Height : 0.7 ,
-Type : [ ' bug ' ],
-},
-{
-name : ' Charmander ' ,
-Height : 0.6 ,
-Type : [ ' fire ' ],
-},
-{
-name : ' Squirtle ' ,
-Height : 0.5 ,
-Type : [ ' water ' ],
-}
+var repository = [
+  {
+    creatureName : 'Metapod',
+    creatureHeight : 0.7,
+    creatureAbilities : ['Shed-skin'],
+    creatureTypes : ['Bug'],
+  },
+  {
+    creatureName : 'Charmander',
+    creatureHeight : 2.00,
+    creatureAbilities : ['Blaze', 'Solar-power'],
+    creatureTypes : ['Fire'],
+  },
+  {
+    creatureName : 'Squirtle',
+    creatureHeight : 0.5,
+    creatureAbilities : ['Rain-dish', 'Torrent'],
+    creatureTypes : ['Monster', 'Water  1'],
+  },
 ];
-  console.log(pokemonList);
+
+for ( let i = 0; i < repository.length; i++) {
+  document.write('<h2>' + repository[i].creatureName + '</h2>' + ' Height: ' + repository[i].creatureHeight)
+if (repository[i].creatureHeight >= 2.00) {
+  document.write(' (Wow that\'s big!)' )
+  }
+}
