eb384375c7243b114bd01359a30a771d810e2b95
Tom Viemont <46653205+tomheartstacos@users.noreply.github.com>
2020-04-29 10:38:18 +0100

    Wrapped var in Iffy

--- scripts.js
+++ scripts.js
@@ -1,26 +1,27 @@
-var repositoryList = [
-  {
-    creatureName : 'Metapod',
-    creatureHeight : 0.7,
-    creatureAbilities : ['Shed-skin'],
-    creatureTypes : ['Bug'],
-  },
-  {
-    creatureName : 'Charmander',
-    creatureHeight : 2.00,
-    creatureAbilities : ['Blaze', 'Solar-power'],
-    creatureTypes : ['Fire'],
-  },
-  {
-    creatureName : 'Squirtle',
-    creatureHeight : 0.5,
-    creatureAbilities : ['Rain-dish', 'Torrent'],
-    creatureTypes : ['Monster', 'Water  1'],
-  },
-];
 var pokemonRepository = (function () {
-  var repositoryList = [];
-}
-)();
+  var repositoryList = [
+    { creatureName : 'Metapod', creatureHeight : 0.7,  creatureAbilities : ['Shed-skin'], creatureTypes : ['Bug'] },
+    { creatureName : 'Charmander', creatureHeight : 6, creatureAbilities : ['Blaze', 'Solar-power'], creatureTypes : ['Fire'] },
+    { creatureName : 'Squirtle', creatureHeight : 0.5, creatureAbilities : ['Rain-dish', 'Torrent'], creatureTypes : ['Monster', 'Water  1'] },
+  ];
+  function getAll() {
+    return repositoryList;
+  }
 
-pokemonRepository.forEach(element => console.log(element));
+  function add(pokemon) {
+    repositoryList.push(pokemon);
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+};
+})();
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ creatureName: 'Tornadus', creatureHeight: 1.5, types: ['flying'] });
+document.write(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+
+;
+});
