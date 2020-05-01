89bfe81f1c0aa951947102ffa380af6f83b4f64a
Tom Viemont <46653205+tomheartstacos@users.noreply.github.com>
2020-05-01 17:09:27 +0100

    Writes details to log

--- scripts.js
+++ scripts.js
@@ -1,38 +1,70 @@
-var pokemonRepository = (function () {
-var repositoryList = [
-{ creatureName : 'Metapod', creatureHeight : 0.7, creatureAbilities : ['Shed-skin'], creatureTypes : ['Bug'] },
-{ creatureName : 'Charmander', creatureHeight : 6, creatureAbilities : ['Blaze', 'Solar-power'], creatureTypes : ['Fire'] },
-{ creatureName : 'Squirtle', creatureHeight : 0.5, creatureAbilities : ['Rain-dish', 'Torrent'], creatureTypes : ['Monster', 'Water 1'] },
-];
-function getAll() {
-return repositoryList;
-}
+var $pokemonList = document.querySelector('ul');
 
-function add(pokemon) {
-repositoryList.push(pokemon);
-}
+var pokemonRepository = (function(){
+  var repository = [
+    {
+      name: 'Bulbasaur',
+      index: 1,
+      height: 0.7,
+      weight: 6.9,
+      types: ['grass','poison']
+    },
+    {
+      name: 'Ivysaur',
+      index: 2,
+      height: 1.0,
+      weight: 13.0,
+      types: ['grass','poison']
+    },
+    {
+      name: 'Venusaur',
+      index: 3,
+      height: 2.0,
+      weight: 100.0,
+      types: ['grass','poison']
+    }
+  ];
 
-function addListItem(pokemon = {}) {
-  var pokemonRepository = document.querySelector('.pokemon-list');
-  var $listItem = document.createElement('li');
-  var button = document.createElement('button');
-  button.innerText = creatureName;
-  button.classList.add('my-class');
-  $listItem.appendChild(button);
-  pokemonList.appendChild($listItem);
-  button.addEventListener('click', function(event) {
-    showDetails(pokemon);
-  });//
-}
+  //Function to add new Pokemon data
+  function add(pokemon){
+    //Must be an 'object' type
+    if (typeof pokemon !== 'object'){
+      return 'Not a valid input'
+    }else{
+    repository.push(pokemon);
+    }
+  }
 
-return {
-add: add,
-getAll: getAll
-};
-})()
-pokemonRepository.getAll().forEach(function(repository){
-document.write('<p>' + 'name: ' + repository.creatureName + '<br>');
-document.write('height: ' + repository.creatureHeight + '<br>');
-document.write('types: ' + repository.creatureTypes + '<br>');
-document.write('abilities: ' + repository.creatureAbilities + '<br>' + '<p>');
+  //Function to pull all Pokemon data
+  function getAll(){
+    return repository;
+  }
+
+  function addListItem(pokemon){
+    var listItem = document.createElement('li');
+    var button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('new-style');
+    listItem.appendChild(button);
+    $pokemonList.appendChild(listItem)
+    button.addEventListener('click', function (){
+      showDetails(pokemon)
+    })
+  }
+
+  function showDetails(pokemon){
+    console.log(pokemon)
+  }
+
+  return{
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+    showDetails: showDetails
+  };
+})();
+
+//Creates list of Pokemon with Pokemon's name on the button
+pokemonRepository.getAll().forEach(function(currentItem){
+  pokemonRepository.addListItem(currentItem);
 })
