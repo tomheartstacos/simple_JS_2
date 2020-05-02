d430c87fba0ca251046e39e4816b46456cb0cb2c
Tom Viemont <46653205+tomheartstacos@users.noreply.github.com>
2020-05-02 16:06:41 +0100

    With JSON

--- scripts.js
+++ scripts.js
@@ -1,70 +1,102 @@
-var $pokemonList = document.querySelector('ul');
-
+// Create variable and assign it to an empty array
+// Wrap it in an IFFE
 var pokemonRepository = (function(){
-  var repository = [
-    {
-      name: 'Bulbasaur',
-      index: 1,
-      height: 0.7,
-      weight: 6.9,
-      types: ['grass','poison']
-    },
-    {
-      name: 'Ivysaur',
-      index: 2,
-      height: 1.0,
-      weight: 13.0,
-      types: ['grass','poison']
-    },
-    {
-      name: 'Venusaur',
-      index: 3,
-      height: 2.0,
-      weight: 100.0,
-      types: ['grass','poison']
-    }
-  ];
+  var repository = [];
+  var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   //Function to add new Pokemon data
-  function add(pokemon){
-    //Must be an 'object' type
-    if (typeof pokemon !== 'object'){
-      return 'Not a valid input'
-    }else{
+  function add(pokemon) {
     repository.push(pokemon);
-    }
   }
 
   //Function to pull all Pokemon data
-  function getAll(){
+  function getAll() {
     return repository;
   }
 
-  function addListItem(pokemon){
-    var listItem = document.createElement('li');
-    var button = document.createElement('button');
-    button.innerText = pokemon.name;
-    button.classList.add('new-style');
-    listItem.appendChild(button);
-    $pokemonList.appendChild(listItem)
-    button.addEventListener('click', function (){
-      showDetails(pokemon)
-    })
+// Function to add new listing for each pokemon
+  function addListItem(pokemon) {
+    var $pokemonList = document.querySelector('ul');
+    // To create a new li-element that contains a button for each pokemon:
+    var $listItem = document.createElement('li');
+    var $button = document.createElement('button');
+    // To append the list item to the unordered list as its child:
+    $pokemonList.appendChild($listItem);
+    // To append the button to the list item as its child:
+    $listItem.appendChild($button);
+    // The button shows the name from the current pokemon:
+    $button.innerText = pokemon.name;
+    $button.classList.add('list-button');
+    // To give the button a custom button style from styles.css to overwrite default styling:
+    $listItem.classList.add('button');
+    // To give the button a function when it's clicked:
+    $button.addEventListener('click', function(event) {
+    // Calls function showDetails to show attributes from each pokemon:
+    showDetails(pokemon);
+  })
+}
+
+// Function to load pokemon list from apiUrl:
+function loadList() {
+  return fetch(apiUrl).then(function (response) {
+    // JSON: the most common data format used when exchanging data with external sources.
+    return response.json();
+  // If the promise is resolved, then (function(passed parameter) {...
+  }).then(function (json) {
+    json.results.forEach(function (item) {
+      var pokemon = {
+        name: item.name,
+        detailsUrl: item.url
+      };
+      add(pokemon);
+    });
+  // If the promise is rejected, catch (function(passed parameter) {...
+  }).catch(function(error) {
+    console.error(error);
+  })
+}
+
+// Function to load details for each pokemon:
+function loadDetails(item) {
+  var url = item.detailsUrl;
+  return fetch(url).then(function(response) {
+    return response.json();
+  }).then(function (details) {
+    // To add the details to each item:
+    item.imageUrl = details.sprites.front_default;
+    item.height = details.height;
+    item.types = Object.keys(details.types);
+  }).catch(function (error) {
+    console.error(error);
+  });
+}
+ // Function to return Pokedex object array:
+function catchAll() {
+return repository;
+}
+
+// Function to show details of each pokemon in alert window:
+  function showDetails(item) {
+    pokemonRepository.loadDetails(item).then(function () {
+    console.log(item);
+    alert('Height:' + ' ' + item.height + ' ' + 'Type: ' + ' ' + item.types);
+    });
   }
 
-  function showDetails(pokemon){
-    console.log(pokemon)
-  }
-
-  return{
-    add: add,
-    getAll: getAll,
-    addListItem: addListItem,
-    showDetails: showDetails
-  };
+// To return the values wich can be accessed to outside the IIFE:
+return {
+  add: add,
+  getAll: getAll,
+  addListItem: addListItem,
+  loadList: loadList,
+  loadDetails: loadDetails,
+  showDetails: showDetails
+};
 })();
 
-//Creates list of Pokemon with Pokemon's name on the button
-pokemonRepository.getAll().forEach(function(currentItem){
-  pokemonRepository.addListItem(currentItem);
-})
+// To create list of pokemon with pokemon's name on the button:
+pokemonRepository.loadList().then(function() {
+  pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon);
+  });
+});
